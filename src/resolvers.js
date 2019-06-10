50eea07b5a1bc9dc39a9eb6ba0537a4eab9e469c
Seth Davenport <seth@rangle.io>
2019-06-10 18:16:40 -0400

    CRUD

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,4 +1,6 @@
-const links = [{
+const { find } = require('lodash');
+
+let links = [{
   id: 'link-0',
   url: 'http://yodawg.com',
   description: 'I heard you like resolvers. So I put links in your resolvers so you can link while you resolve',
@@ -10,8 +12,32 @@ module.exports = {
       info: () => 'Yo dawg I heard you like graphs.',
       feed: () => links,
     },
+    Mutation: {
+      post: (parent, args) => {
+        const link = {
+          id: `link-${links.length+1}`,
+          description: args.description,
+          url: args.url,
+        };
+        links.push(link);
+        return link;
+      },
+      update: (parent, { id, url, description }) => {
+        const updatee = find(links, { id });
+        if (!!url) { updatee.url = url; }
+        if (!!description) { updatee.description = description; }
+        return updatee;
+      },
+      destroy: (parent, args) => {
+        const originalCount = links.length;
+        links = links.filter(link => link.id !== args.id);
+        return links.length < originalCount;
+      }
+    },
     Link: {
-      url: () => 'http://yodawg.com/'
+      id: parent => parent.id,
+      url: parent => parent.url,
+      description: parent => parent.description,
     },
   },
 };
