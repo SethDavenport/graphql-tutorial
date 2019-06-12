77ebaf8cf0cb8d6aaa02266f2b9b47469141716f
Seth Davenport <seth.e.davenport@gmail.com>
2019-06-12 17:50:31 -0400

    paging and filtering

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,6 +1,31 @@
 const info = () => 'Yo dawg I heard you like graphs.';
 
-const feed = (root, args, { db }) => db('links');
+const feed = async (root, { filter, offset, limit }, { db }) => {
+  let query = db('links')
+    .orderBy('created_at', 'desc');
+
+  let countQuery = db('links')
+    .count('*');
+
+  if (!!filter) {
+    query = query.where('description', 'LIKE', `%${filter}%`);
+    countQuery = countQuery.where('description', 'LIKE', `%${filter}%`);
+  }
+
+  if (offset !== undefined && offset !== null) {
+    query = query.offset(offset);
+  }
+
+  if (limit !== undefined && limit !== null) {
+    query = query.limit(limit);
+  }
+
+  const [{ count }] = await countQuery;
+  return {
+    links: await query,
+    count,
+  };
+}
 
 module.exports = {
   info,
